9cc0a901d43eb45a188e0d05e14503b1e62dca44
Mark Old <dlom234@gmail.com>
2017-11-24 18:05:11 -0800

    Add object transformation utilities

---
+++ src/transform.js
@@ -0,0 +1,56 @@
+import hasOwnProp from "has-own-prop";
+
+const preparePlugin = plugin => {
+	const blacklistSet = new Set(plugin.blacklist);
+	const transforms = plugin.transforms.map(transform => {
+		if (typeof transform[1] === "function") {
+			return transform;
+		}
+		blacklistSet.add(transform[0]);
+		return [transform[0], incoming => {
+			return [
+				[transform[1], incoming[transform[0]]]
+			];
+		}];
+	});
+	const blacklist = Array.from(blacklistSet);
+	return { transforms, blacklist };
+};
+
+const generateMappings = (transforms, incoming) => {
+	return transforms.filter(transform => {
+		return hasOwnProp(incoming, transform[0]);
+	}).map(transform => {
+		return transform[1](incoming);
+	}).reduce((a, b) => {
+		return a.concat(b);
+	});
+};
+
+const transformOne = (incoming, mappings, blacklist) => {
+	const outgoing = {};
+	for (const mapping of mappings) {
+		outgoing[mapping[0]] = mapping[1];
+	}
+	for (const property of Object.keys(incoming)) {
+		if (!hasOwnProp(outgoing, property)) {
+			outgoing[property] = incoming[property];
+		}
+	}
+	for (const property of blacklist) {
+		if (hasOwnProp(outgoing, property)) {
+			delete outgoing[property];
+		}
+	}
+	return outgoing;
+};
+
+const transformProperties = (incoming, plugins) => {
+	return plugins.reduce((inProgress, plugin) => {
+		const preparedPlugin = preparePlugin(plugin);
+		const mappings = generateMappings(preparedPlugin.transforms, inProgress);
+		return transformOne(inProgress, mappings, preparedPlugin.blacklist);
+	}, incoming);
+};
+
+export { preparePlugin, generateMappings, transformOne, transformProperties };
---
+++ test/transform.test.js
@@ -0,0 +1,104 @@
+import test from "ava";
+
+import { preparePlugin, transformOne, generateMappings, transformProperties } from "../src/transform";
+
+const incoming = {
+	a: "Property 1",
+	c: "Property 2",
+	e: "Property 3",
+	q: 7,
+	bad: "Bad Property",
+	other: "Other Property"
+};
+
+const plugin = {
+	transforms: [
+		["a", "b"],
+		["c", "d"],
+		["e", incoming => [["f", `${incoming.e} is cool`], ["z", incoming.q + 3]]]
+	],
+	blacklist: [
+		"e",
+		"bad"
+	]
+};
+
+const final = {
+	b: "Property 1",
+	d: "Property 2",
+	f: "Property 3 is cool",
+	q: 7,
+	z: 10,
+	other: "Other Property"
+};
+
+const plugin2 = {
+	transforms: [
+		["d", "m"],
+		["not", "there"],
+		["bad", "good"],
+		["other", incoming => [["other", `${incoming.other} changed!`]]]
+	],
+	blacklist: [
+		"q"
+	]
+};
+
+const final2 = {
+	b: "Property 1",
+	m: "Property 2",
+	f: "Property 3 is cool",
+	z: 10,
+	other: "Other Property changed!"
+};
+
+test("preparePlugin()", t => {
+	const preparedPlugin = preparePlugin(plugin);
+	const preparedTransforms = preparedPlugin.transforms;
+	const preparedBlacklist = preparedPlugin.blacklist;
+
+	t.true(Array.isArray(preparedTransforms));
+	t.is(preparedTransforms.length, plugin.transforms.length);
+	for (const transform of preparedTransforms) {
+		t.true(Array.isArray(transform));
+		t.is(transform.length, 2);
+		t.is(typeof transform[0], "string");
+		t.is(typeof transform[1], "function");
+	}
+
+	t.true(Array.isArray(preparedBlacklist));
+	t.deepEqual(preparedBlacklist.sort(), plugin.transforms.filter(transform => {
+		return typeof transform[1] === "string";
+	}).map(transform => transform[0]).concat(plugin.blacklist).sort());
+});
+
+test("generateMappings()", t => {
+	const preparedPlugin = preparePlugin(plugin);
+	const mappings = generateMappings(preparedPlugin.transforms, incoming);
+
+	t.true(Array.isArray(mappings));
+	for (const mapping of mappings) {
+		t.true(Array.isArray(mapping));
+		t.is(mapping.length, 2);
+		t.is(typeof mapping[0], "string");
+	}
+});
+
+test("transformOne()", t => {
+	const preparedPlugin = preparePlugin(plugin);
+	const mappings = generateMappings(preparedPlugin.transforms, incoming);
+	const outgoing = transformOne(incoming, mappings, preparedPlugin.blacklist);
+
+	t.is(typeof outgoing, "object");
+	t.deepEqual(outgoing, final);
+});
+
+test("transformProperties()", t => {
+	const plugins = [
+		plugin,
+		plugin2
+	];
+	const outgoing = transformProperties(incoming, plugins);
+	t.is(typeof outgoing, "object");
+	t.deepEqual(outgoing, final2);
+});
